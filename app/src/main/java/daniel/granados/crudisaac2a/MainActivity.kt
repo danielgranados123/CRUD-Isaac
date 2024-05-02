91f9ad6d315b11b5c7b8db8cd2e36abdcd4478af
Daniel Granados <20200008@ricaldone.edu.sv>
2024-05-02 10:47:07 -0600

    Primer CRUD

--- app/src/main/java/daniel/granados/crudisaac2a/MainActivity.kt
+++ app/src/main/java/daniel/granados/crudisaac2a/MainActivity.kt
@@ -1,10 +1,16 @@
 package daniel.granados.crudisaac2a
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import modelo.ClaseConexion
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,31 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //1-Mandar a llamar a todos los elementos de la pantalla
+        val txtNombreUsuario = findViewById<EditText>(R.id.txtNombreProducto)
+        val txtPrecio = findViewById<EditText>(R.id.txtPrecioProducto)
+        val txtCantidad = findViewById<EditText>(R.id.txtCantidadProducto)
+        val btnAgregar = findViewById<Button>(R.id.btnAgregar)
+
+        //2- Programar el boton
+        btnAgregar.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO){
+                //Guardar datos
+                //1-Crear objeto de la clase conexión
+
+                val claseConexion = ClaseConexion().cadenaConexion()
+
+                //2- Crear variable que contenga un PreparedStatement
+                val addProducto = claseConexion?.prepareStatement("insert into tbProductos(nombreProducto, precio, cantidad) values(?, ?, ?)")!!
+
+                addProducto.setString(1, txtNombreUsuario.text.toString())
+                addProducto.setInt(2, txtPrecio.text.toString().toInt())
+                addProducto.setInt( 3, txtCantidad.text.toString().toInt())
+                addProducto.executeUpdate()
+        }
+
         }
     }
 }
---
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -0,0 +1,28 @@
+package modelo
+
+import java.lang.Exception
+import java.sql.Connection
+import java.sql.DriverManager
+
+class ClaseConexion {
+
+    fun cadenaConexion(): Connection? {
+
+        try {
+            val url = "jdbc:oracle:thin:@10.10.1.115:1521:xe"
+            val usuario = "system"
+            val contrasena = "desarrollo"
+
+            val connection = DriverManager.getConnection(url, usuario, contrasena)
+
+            return connection
+        }
+
+        catch (e: Exception){
+            println("Este es el error: $e")
+            return null
+        }
+
+    }
+
+}
